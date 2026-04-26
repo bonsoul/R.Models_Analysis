932d6e05f71702132a1efcf2b6f3ff45b96b4dd4
bonsoul <bonsoul24@gmail.com>
2026-04-26 22:13:57 +0300

    Modelling

--- Homabay Modelling.R
+++ Homabay Modelling.R
@@ -13,3 +13,19 @@ Summary(df)
 
 str(df)
 
+Surv(time = df$time, event = df$monthsantibodytest)
+
+
+table(df$monthsantibodytest, useNA = "ifany")
+
+
+df$event <- ifelse(df$monthsantibodytest == "positive", 1,
+                   ifelse(df$monthsantibodytest == "negative", 0, NA))
+
+df_clean <- df %>%
+  filter(!is.na(event))
+
+Surv(time = df$time, event = df$event)
+
+
+
