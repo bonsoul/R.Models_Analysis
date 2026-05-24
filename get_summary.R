b474b07a21c1a41efb25e88e51cd0f8f4d3d208d
bonsoul <bonsoul24@gmail.com>
2026-05-25 01:27:32 +0300

    Summary

--- get_summary.R
+++ get_summary.R
@@ -11,3 +11,22 @@ get_summary_stats(data = Wage, age, wage,
                   type = "mean_sd")
 get_summary_stats(data = Wage, age,wage,
                   type = "five_number")
+
+
+
+#produce a tidy frequency table
+
+bla <- table(Wage$education, Wage$jobclass)
+
+
+bla |> prop.table(margin =1)
+
+
+Wage |> freq_table(education, jobclass)
+
+
+Wage |> freq_table(jobclass, education)
+
+
+table(Wage$education, Wage$jobclass) |>
+  prop.table(margin = 2)
