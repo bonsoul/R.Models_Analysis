9fea83a99f572791e65dbefbfc29ca425dfc49b9
bonsoul <bonsoul24@gmail.com.com>
2026-06-13 17:33:21 +0300

    Modelling

--- RSTATIX.R
+++ RSTATIX.R
@@ -14,3 +14,18 @@ tbl_summary(
   Wage,
   include = c(age, wage)
 )
+
+
+get_summary_stats(data = Wage, age, wage,
+                  type = 'five_number')
+
+
+get_summary_stats(data = Wage, age, wage,
+                  type = 'median')
+
+
+Wage |> 
+  group_by(health, jobclass) |>
+  get_summary_stats(
+    wage,age, show = c("mean","sd","median","iqr")
+  )
