c506871354514ced396c33c091e1fa57acefcbc7
bonsoul <bonsoul24@gmail.com.com>
2026-07-31 20:27:07 +0300

    modelling

--- Ecotourism.R
+++ Ecotourism.R
@@ -1,5 +1,14 @@
+library(dplyr)
+library(lubridate)
+library(tidytuesdayR)
+library(tidyverse)
+
+
+#loading the datasets
+
 tuesdata <- tidytuesdayR::tt_load(2026, week = 30)
 
 occurrences <- tuesdata$occurrences
 tourism <- tuesdata$tourism
 weather <- tuesdata$weather
+
