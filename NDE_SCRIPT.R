67617ded4acd95df5de1fd9a8e72de94cf5cc6cb
bonsoul <bonsoul24@gmail.com>
2026-08-06 16:38:09 +0300

    modelling

---
+++ NDE_SCRIPT.R
@@ -0,0 +1,8 @@
+
+##libraries
+library(dplyr)
+library(tidytuesdayR)
+library(tidyverse)
+
+##Importing the datset
+nde_experiences <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-07-21/nde_experiences.csv')
